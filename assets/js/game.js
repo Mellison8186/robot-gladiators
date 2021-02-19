ed3e0529467b89eb327b7bc9400a007addea30a1
maribels_are_ringing <maribel.duron86@gmail.com>
2021-02-18 21:02:18 -0700

    Adding random order fighting function

--- assets/js/game.js
+++ assets/js/game.js
@@ -83,12 +83,17 @@ promptFight = promptFight.toLocaleLowerCase();
 }
 // fight function (now with parameter for enemy's object holding name, health, and attack values)
 var fight = function(enemy) {
-  while (playerInfo.health > 0 && enemy.health > 0) {
+  // keep track of who goes first
+var isPlayerTurn = true;
+if (Math.random() > 0.5) {
+  isPlayerTurn = false;
+} 
+while (playerInfo.health > 0 && enemy.health > 0) {
+  if (isPlayerTurn) {
     // ask player if they'd like to fight or skip using fightOrSkip function
-    if (fightOrSkip()){
+    if (fightOrSkip()) {
       // if true, leave fight by breaking loop
       break;
-    }
     }
     var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
@@ -111,6 +116,42 @@ var fight = function(enemy) {
 
       // award player money for winning
       playerInfo.money = playerInfo.money + 20;
+
+              // leave while() loop since enemy is dead
+              break;
+            } else {
+              window.alert(enemy.name + " still has " + enemy.health + " health left.");
+            }
+            // player gets attacked first
+          } else {
+            var damage = randomNumber(enemy.attack - 3, enemy.attack);
+      ​
+            // remove player's health by subtracting the amount we set in the damage variable
+            playerInfo.health = Math.max(0, playerInfo.health - damage);
+            console.log(
+              enemy.name +
+                " attacked " +
+                playerInfo.name +
+                ". " +
+                playerInfo.name +
+                " now has " +
+                playerInfo.health +
+                " health remaining."
+            );
+      ​
+            // check player's health
+            if (playerInfo.health <= 0) {
+              window.alert(playerInfo.name + " has died!");
+              // leave while() loop if player is dead
+              break;
+            } else {
+              window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
+            }
+          }
+          // switch turn order for next round
+          isPlayerTurn = !isPlayerTurn;
+        }
+      };
 
       // ask if player wants to use the store before next round
       var storeConfirm = window.confirm('The fight is over, visit the store before the next round?');
